3ed3cf1db75415ad7a30d7802243d8c4eb4b01bf
Egor Evseev <egor.evseev1312@gmail.com>
2026-03-20 21:04:47 +0400

    Added first repo and Alchemy

---
+++ fastapi_app/src/models/categories.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, Text, Boolean, DateTime
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+
+class Category(Base):
+    __tablename__ = "blog_category"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(256))
+    description = Column(Text)
+    slug = Column(String, unique=True)
+    is_published = Column(Boolean, default=True)
+    created_at = Column(DateTime)
