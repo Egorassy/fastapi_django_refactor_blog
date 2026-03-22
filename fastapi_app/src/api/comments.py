d6bd9fec7c9ea8e77bb2d626e320f71756b3a3fe
Egor Evseev <egor.evseev1312@gmail.com>
2026-03-22 23:30:07 +0400

    Improved CRUD

--- fastapi_app/src/api/comments.py
+++ fastapi_app/src/api/comments.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from datetime import datetime
 from typing import List
 from ..schemas.comments import CommentCreate, CommentRead
@@ -14,6 +14,13 @@ def get_all():
     return db
 
 
+@router.get("/{item_id}", response_model=CommentRead)
+def get_one(item_id: int):
+    if item_id < 1 or item_id > len(db):
+        raise HTTPException(status_code=404, detail="Comment not found")
+    return db[item_id - 1]
+
+
 @router.post("/", response_model=CommentRead)
 def create(item: CommentCreate):
     global counter
@@ -25,3 +32,26 @@ def create(item: CommentCreate):
     db.append(new_item)
     counter += 1
     return new_item
+
+
+@router.put("/{item_id}", response_model=CommentRead)
+def update(item_id: int, item: CommentCreate):
+    if item_id < 1 or item_id > len(db):
+        raise HTTPException(status_code=404, detail="Comment not found")
+
+    updated = {
+        "id": item_id,
+        "created_at": db[item_id - 1]["created_at"],
+        **item.dict()
+    }
+    db[item_id - 1] = updated
+    return updated
+
+
+@router.delete("/{item_id}")
+def delete(item_id: int):
+    if item_id < 1 or item_id > len(db):
+        raise HTTPException(status_code=404, detail="Comment not found")
+
+    db.pop(item_id - 1)
+    return {"ok": True}
--- fastapi_app/src/api/locations.py
+++ fastapi_app/src/api/locations.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from datetime import datetime
 from typing import List
 from ..schemas.locations import LocationCreate, LocationRead
@@ -14,6 +14,13 @@ def get_all():
     return db
 
 
+@router.get("/{item_id}", response_model=LocationRead)
+def get_one(item_id: int):
+    if item_id < 1 or item_id > len(db):
+        raise HTTPException(status_code=404, detail="Location not found")
+    return db[item_id - 1]
+
+
 @router.post("/", response_model=LocationRead)
 def create(item: LocationCreate):
     global counter
@@ -25,3 +32,26 @@ def create(item: LocationCreate):
     db.append(new_item)
     counter += 1
     return new_item
+
+
+@router.put("/{item_id}", response_model=LocationRead)
+def update(item_id: int, item: LocationCreate):
+    if item_id < 1 or item_id > len(db):
+        raise HTTPException(status_code=404, detail="Location not found")
+
+    updated = {
+        "id": item_id,
+        "created_at": db[item_id - 1]["created_at"],
+        **item.dict()
+    }
+    db[item_id - 1] = updated
+    return updated
+
+
+@router.delete("/{item_id}")
+def delete(item_id: int):
+    if item_id < 1 or item_id > len(db):
+        raise HTTPException(status_code=404, detail="Location not found")
+
+    db.pop(item_id - 1)
+    return {"ok": True}
