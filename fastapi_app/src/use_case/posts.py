0d72c7065e0a29998e0ca06884e75697c55236ab
Egor Evseev <egor.evseev1312@gmail.com>
2026-04-28 00:05:19 +0400

    Fixed category and location validation for posts

--- fastapi_app/src/use_case/posts.py
+++ fastapi_app/src/use_case/posts.py
@@ -1,7 +1,13 @@
 from sqlalchemy.orm import Session
 
 from ..infrastructure.repositories.posts import PostRepository
-from ..infrastructure.module.exceptions import NotFoundError as RepoNotFoundError, IntegrityDatabaseError
+from ..infrastructure.repositories.categories import CategoryRepository
+from ..infrastructure.repositories.locations import LocationRepository
+
+from ..infrastructure.module.exceptions import (
+    NotFoundError as RepoNotFoundError,
+    IntegrityDatabaseError,
+)
 
 from ..core.exceptions.http import NotFoundError, ConflictError
 
@@ -10,6 +16,8 @@ class PostUseCase:
 
     def __init__(self):
         self.repo = PostRepository()
+        self.category_repo = CategoryRepository()
+        self.location_repo = LocationRepository()
 
     def get_all(self, db: Session):
         return self.repo.get_all(db)
@@ -21,12 +29,16 @@ class PostUseCase:
             raise NotFoundError(f"Post {item_id} not found", code="post_not_found")
 
     def create(self, db: Session, data: dict):
+        self._check_relations(db, data)
+
         try:
             return self.repo.create(db, data)
         except IntegrityDatabaseError:
             raise ConflictError("Post already exists", code="post_conflict")
 
     def update(self, db: Session, item_id: int, data: dict):
+        self._check_relations(db, data)
+
         try:
             return self.repo.update(db, item_id, data)
         except RepoNotFoundError:
@@ -37,3 +49,24 @@ class PostUseCase:
             self.repo.delete(db, item_id)
         except RepoNotFoundError:
             raise NotFoundError(f"Post {item_id} not found", code="post_not_found")
+
+    def _check_relations(self, db: Session, data: dict):
+        location_id = data.get("location_id")
+        if location_id is not None:
+            try:
+                self.location_repo.get_by_id(db, location_id)
+            except RepoNotFoundError:
+                raise NotFoundError(
+                    f"Location {location_id} not found",
+                    code="location_not_found"
+                )
+
+        category_id = data.get("category_id")
+        if category_id is not None:
+            try:
+                self.category_repo.get_by_id(db, category_id)
+            except RepoNotFoundError:
+                raise NotFoundError(
+                    f"Category {category_id} not found",
+                    code="category_not_found"
+                )
