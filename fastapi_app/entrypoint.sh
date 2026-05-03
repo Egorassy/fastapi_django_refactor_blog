6ce16464a0b7d65bb2eeab0e43539d4330c53410
Egor Evseev <egor.evseev1312@gmail.com>
2026-05-04 01:07:45 +0400

    improved readability

--- fastapi_app/entrypoint.sh
+++ fastapi_app/entrypoint.sh
@@ -2,7 +2,11 @@
 set -e
 
 echo "Waiting for PostgreSQL..."
-until PGPASSWORD="$POSTGRES_PASSWORD" pg_isready -h "$POSTGRES_HOST" -p "$POSTGRES_PORT" -U "$POSTGRES_USER" >/dev/null 2>&1; do
+until PGPASSWORD="$POSTGRES_PASSWORD" pg_isready \
+  -h "$POSTGRES_HOST" \
+  -p "$POSTGRES_PORT" \
+  -U "$POSTGRES_USER" >/dev/null 2>&1
+do
   sleep 1
 done
 
